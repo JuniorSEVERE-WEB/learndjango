230da2013594b2492ffc2d988ccdd52e95cd294f
Junior SEVERE <severejunior2017@gmail.com>
2025-04-26 10:29:58 -0700

    dynamic path & if cond

--- monthsChallenge/views.py
+++ monthsChallenge/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 def index(request):
     return HttpResponse("Welcome to the challenge of the all months!")
 
 def months(request, month):
-    return HttpResponse(f" We are in {month}")
+    if month == "january":
+        message_month = "We are in january, we ate rice"
+    elif month == "february":
+        message_month = "We are in february"
+    else:
+        return HttpResponseNotFound("Invalid month!")     
+    return HttpResponse(message_month)
