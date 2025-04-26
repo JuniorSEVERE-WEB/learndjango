1affba74043b213fbe3711fe8ced9f7e2b93214d
Junior SEVERE <severejunior2017@gmail.com>
2025-04-26 11:06:53 -0700

    dictionary & dynamic view logic

--- monthsChallenge/views.py
+++ monthsChallenge/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
+dict_months = {
+    "january": " We are in january",
+    "februray": " We are in february",
+    "march": "We are in march",
+    "april": "We are in april",
+    "may": "We are in may",
+    "june": "We are in june",
+    "july": "We are in july",
+    "august": "We are in august",
+    "september": "We are in september",
+    "october": "We are in october",
+    "november": "We are in november",
+    "december": "we are in december"
+}
+
 def index(request):
     return HttpResponse("Welcome to the challenge of the all months!")
 
@@ -9,12 +24,8 @@ def months_by_int(request, month):
     return HttpResponse(month)
     
 def months_by_str(request, month):
-    if month == "january":
-        message_month = "We are in january, we ate rice"
-    elif month == "february":
-        message_month = "We are in february"
-    elif month == "march":
-        message_month = "We are in march"    
-    else:
-        return HttpResponseNotFound("Invalid month!")     
-    return HttpResponse(message_month)
+    try:
+        message_month = dict_months[month]
+        return HttpResponse(message_month)
+    except:
+        return HttpResponseNotFound("Invalid month!")
